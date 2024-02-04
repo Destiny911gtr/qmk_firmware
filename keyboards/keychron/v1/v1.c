e471da35bc48fbacae6125f2e40d4ea4c3856b54
Destiny911gtr <dhanush911gtr@gmail.com>
2024-02-04 18:50:06 +0530

    RGB Changes and QMK version bump

--- keyboards/keychron/v1/v1.c
+++ keyboards/keychron/v1/v1.c
@@ -33,13 +33,12 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
     }
     if (index == 0) {
         default_layer_set(1UL << (active ? 2 : 0));
-        // if (active) {
-        //     rgb_matrix_mode(RGB_MATRIX_SIGNALRGB);
-        // } else {
-        //     rgb_matrix_sethsv(6, 255, 255);
-        //     rgb_matrix_set_speed_noeeprom(127);
-        //     rgb_matrix_mode(RGB_MATRIX_RIVERFLOW);
-        // }
+        if (active) {
+            rgb_matrix_enable_noeeprom();
+            rgb_matrix_mode(RGB_MATRIX_RIVERFLOW);
+        } else {
+            rgb_matrix_disable_noeeprom();
+        }
     }
     return true;
 }
