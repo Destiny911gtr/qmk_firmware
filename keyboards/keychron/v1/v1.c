970b46d78b4108192405f30a901f0debc38876f0
Destiny911gtr <dhanush911gtr@gmail.com>
2024-02-04 18:53:36 +0530

    Dont write RGB changes to EEPROM

--- keyboards/keychron/v1/v1.c
+++ keyboards/keychron/v1/v1.c
@@ -35,7 +35,7 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
         default_layer_set(1UL << (active ? 2 : 0));
         if (active) {
             rgb_matrix_enable_noeeprom();
-            rgb_matrix_mode(RGB_MATRIX_RIVERFLOW);
+            rgb_matrix_mode_noeeprom(RGB_MATRIX_RIVERFLOW);
         } else {
             rgb_matrix_disable_noeeprom();
         }
